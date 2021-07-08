8fe6c802d41582e442edca1575e9891db870c083
habibieee <nayyul20ti@mahasiswa.pcr.ac.id>
2021-07-09 00:43:40 +0700

    udah pake templete

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,7 +1,8 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Welcome extends CI_Controller
+{
 
 	/**
 	 * Index Page for this controller.
@@ -20,6 +21,49 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('login_admin');
+	}
+
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->model('m_login');
+	}
+
+	function aksi_login()
+	{
+		$username = $this->input->post('username');
+		$password = $this->input->post('password');
+		$where = array(
+			'username' => $username,
+			'password' => md5($password)
+		);
+		$cek = $this->m_login->cek_login("login", $where)->num_rows();
+		if ($cek > 0) {
+			echo "berhasil Login";
+		} else {
+			echo "Username dan password salah !";
+		}
+	}
+
+	public function daftar()
+	{
+		$this->load->view('register_user');
+	}
+
+	public function aksi_daftar()
+	{
+		$username = $this->input->post('username');
+		$password = $this->input->post('password');
+		$data = array(
+			'username' => $username,
+			'password' => md5($password)
+		);
+		$cek = $this->m_login->insert_data("login", $data);
+		if ($cek) {
+			echo "USER Berhasil Tambah !";
+		} else {
+			echo "User Gagal Ditambahkan !";
+		}
 	}
 }
---
+++ application/models/m_login.php
@@ -0,0 +1,15 @@
+<?php
+
+class M_login extends CI_Model
+{
+
+    function cek_login($table, $where)
+    {
+        return $this->db->get_where($table, $where);
+    }
+
+    function insert_data($table, $data)
+    {
+        return $this->db->insert($table, $data);
+    }
+}
